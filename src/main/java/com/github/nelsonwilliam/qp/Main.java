372fa4165e75a872f0a30fa67e9e993b14d4d056
NelsonWilliam <nelsonwilliam@live.com>
2016-11-23 01:43:38 -0200

    Primeiro commit com exemplo do professor

---
+++ src/main/java/com/github/nelsonwilliam/qp/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+package com.github.nelsonwilliam.qp;
+
+/**
+ *
+ * @author NelsonWilliam
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("So...?");
+        System.exit(0);
+    }
+
+}
